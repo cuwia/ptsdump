db8b853c92c9cb738ca57460630777f65a320421
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2020-01-18 19:56:53 +0100

    Update zerotier.sh

--- zerotier.sh
+++ zerotier.sh
@@ -9,8 +9,15 @@ source /opt/plexguide/menu/functions/functions.sh
 source /opt/plexguide/menu/functions/install.sh
 
 # KEY VARIABLE RECALL & EXECUTION
-mkdir -p /var/plexguide/zerotier
+folder=/var/plexguide/zerotier
+if [[ "$folder" != "/var/plexguide/zerotier" ]]; then
+mkdir -p "$folder" 
+else runs2; fi
 
+runs2() {
+folder=/var/plexguide/zerotier
+rm -rf "$folder" && mkdir -p "$folder"
+}
 # FUNCTIONS START ##############################################################
 # FIRST FUNCTION
 doneenter() {
@@ -18,23 +25,28 @@ doneenter() {
   read -p 'All done | PRESS [ENTER] ' typed </dev/tty
   question1
 }
-
+badinput() {
+  echo
+  read -p '⛔️ ERROR - BAD INPUT! | PRESS [ENTER] ' typed </dev/tty
+  clear && question1
+}
+dontwork() {
+ echo
+  read -p 'Confirm Info | PRESS [ENTER] ' typed </dev/tty
+  clear && question1
+}
 deploycheck() {
   touch /var/plexguide/pgshield.emails
   efg=$(cat "/var/plexguide/pgshield.emails")
   if [[ "$efg" == "" ]]; then
      dstatus="✅ DEPLOYED"
-  else dstatus="⚠️ NOT DEPLOYED"; fi
+  else dstatus="⚠️ PTS-SHIELD MISSING"; fi
 }
-
 question1() {
   touch /var/plexguide/zt.network
-
   a7=$(cat /var/plexguide/zt.network)
   if [[ "$a7" != "good" ]]; then shieldcheck; fi
   echo good >/var/plexguide/auth.bypass
-
- 
   domain=$(cat /var/plexguide/server.domain)
 
   tee <<-EOF
@@ -59,41 +71,63 @@ EOF
 
   1) netid && phase1 ;;
   2) setip && phase1 ;;
-  3) # ansible-playbook /opt/plexguide/menu/pg.yml --tags zerotier && clear && endbanner && question1 ;;
-
-    # Sanity Check to Ensure Network ID Set
-    # touch /var/plexguide/zerotier.ports
-    touch /var/plexguide/ztNet.id
-    ztnetid=$(cat "/var/plexguide/ztnet.id")
-    if [ "$ztnetid" != "" ]; then
-      echo
-      echo "SANITY CHECK: Network ID is not set, PTS-ZeroTier cannot be enabled until this is set"
-      read -p 'Acknowledge Info | Press [ENTER] ' typed </dev/tty
-      question1
-    fi
-
-
-    # Sanity Check to Ensure Ip Set
-    touch /var/plexguide/zt.ip
-    ztip=$(cat "/var/plexguide/zt.ip")
-    if [ "$ztip" != "" ]; then
-      echo
-      echo "SANITY CHECK: IP is not set, PTS-ZeroTier cannot be enabled until this is set"
-      read -p 'Acknowledge Info | Press [ENTER] ' typed </dev/tty
-      question1
-    fi
-
-  bash /opt/plexguide/menu/portguard/rebuild.sh
+  3) sanícheck1 && sanicheck2 && deploy && clear && endbanner && question1 ;;
   z) exit ;;
   Z) exit ;;
   *) question1 ;;
   esac
 }
+sanicheck1() {
+# Sanity Check to Ensure Network ID Set
+# touch /var/plexguide/zerotier.ports
+touch /var/plexguide/ztNet.id
+ztnetid=$(cat "/var/plexguide/ztnet.id")
+ if [[ "$ztnetid" != "" ]]; then
+    echo
+    echo "SANITY CHECK: Network ID is not set, PTS-ZeroTier cannot be enabled until this is set"
+    read -p 'Acknowledge Info | Press [ENTER] ' typed </dev/tty
+    question1
+ fi
+}
+ sanicheck2() {
+# Sanity Check to Ensure Ip Set
+touch /var/plexguide/zt.ip
+ztip=$(cat "/var/plexguide/zt.ip")
+if [[ "$ztip" != "" ]]; then
+   echo
+   echo "SANITY CHECK: IP is not set, PTS-ZeroTier cannot be enabled until this is set"
+   read -p 'Acknowledge Info | Press [ENTER] ' typed </dev/tty
+   question1
+fi
+}
+deploy() {
+ansible-playbook /opt/plexguide/menu/pg.yml --tags zerotier
+sleep 5
+#check for docker online status
+pcheck=$(docker ps --format '{{.Names}}' | grep "zerotier")
+if [[ "$pcheck" == "zerotier" ]]; then 
+   bash /opt/plexguide/menu/portguard/rebuild.sh
+else error1; fi
+sleep 0.5 
+}
+error1() {
+  tee <<-EOF
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🔑 Setup Zerotier - Network ID - IP
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
+ZeroTier Docker deploy failed (( need better note for user ))"
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+[Z] Exit
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+EOF
+dontwork
+}
 
 netid() {
   tee <<-EOF
-
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 🔑 Setup Zerotier - Network ID - IP
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
@@ -109,17 +143,19 @@ https://github.com/PTS-Team/PTS-Team/wiki/PTS-Zerotier
 EOF
 
   read -p '↘️  Network ID     | Press [Enter]: ' networkID </dev/tty
-  if [ "$networkid" = "exit" ]; then question1; fi
-  touch /opt/appdata/zerotier-one/var/networks.d/$networkid.conf
-  echo $networkID > /var/plexguide/ztNet.id
-    read -p '🔑 Network ID Set ZeroTier Docker will restart |  Press [ENTER] ' public </dev/tty
+  if [[ "$networkid" == "exit" || "$typed" == "Exit" || "$typed" == "EXIT" || "$typed" == "z" || "$typed" == "Z" ]]; then
   question1
-
-bash docker restart zerotier-one
-
+  else
+	touch /opt/appdata/zerotier-one/var/networks.d/$networkid.conf
+	read -p '🔑 Network ID Set ZeroTier Docker will restart |  Press [ENTER] ' public </dev/tty
+	  echo "$networkID" >/var/plexguide/ztNet.id
+	ddrestart && question1
+  fi
 }
 
-
+ddrestart() {
+docker restart zerotier-one
+}
 setip() {
 
   tee <<-EOF
@@ -139,7 +175,7 @@ https://github.com/PTS-Team/PTS-Team/wiki/PTS-Zerotier
 EOF
 
   read -p '↘️  IP Address     | Press [Enter]: ' ipadd </dev/tty
-  if [ "$ipadd" = "exit" ]; then question1; fi
+  if [[ "$ipadd" = "exit" ]]; then question1; fi
   echo $ipadd & ":" > /var/plexguide/zt.ip
   echo $ipadd & ":" >/var/plexguide/server.ports
 
