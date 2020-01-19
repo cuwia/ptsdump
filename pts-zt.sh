7fe872b0544024d5e53c292b037c88000dc3383d
cuwia <60030952+cuwia@users.noreply.github.com>
2020-01-19 13:08:23 +1100

    Update pts-zt.sh

--- pts-zt.sh
+++ pts-zt.sh
@@ -8,13 +8,7 @@
 source /opt/plexguide/menu/functions/functions.sh
 
 question1() {
-  touch /var/plexguide/auth.bypass
-
-  a7=$(cat /var/plexguide/auth.bypass)
-  if [[ "$a7" != "good" ]]; then shieldcheck; fi
-  echo good >/var/plexguide/auth.bypass
-
-  touch /var/plexguide/pgshield.emails
+  
   mkdir -p /var/plexguide/auth/
   mkdir -p /var/lib/zerotier-one
   domain=$(cat /var/plexguide/server.domain)
